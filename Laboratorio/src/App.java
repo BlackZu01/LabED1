4a2cbd846be591460dab050f7e5b8123dda9de2e
BlackZu01 <citozuluaga44@gmail.com>
2022-08-27 21:19:15 -0500

    Punto 1a realizado, se creó el archivo Clientes ordenadamente por cedulas

--- Laboratorio/src/App.java
+++ Laboratorio/src/App.java
@@ -2,6 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -10,16 +11,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner read = new Scanner(System.in);
 
-        //FillClient(read, "Clientes");
-        Leer(read, "Clientes");
+        // FillClient(read, "Clientes");
+        Ordenar(read, "ClientesTemp");
         read.close();
     }
 
-    public static void FillClient (Scanner read, String filename) {
+    public static void FillClient(Scanner read, String filename) {
         String cedula, nombre, dir, celular, email;
 
         try {
-            FileWriter outFile = new FileWriter(filename+ ".txt", false);
+            FileWriter outFile = new FileWriter(filename + ".txt", false);
             PrintWriter register = new PrintWriter(outFile);
 
             String hayRegistro;
@@ -38,10 +39,10 @@ public class App {
                 System.out.print("Ingrese correo electronico: ");
                 email = read.nextLine();
 
-                if (!cedula.isEmpty() && !nombre.isEmpty() 
-                    && !dir.isEmpty() && !celular.isEmpty() 
-                    && !email.isEmpty()) {
-                    register.println(cedula+ "\t" +nombre+ "\t" +dir+ "\t" +celular+ "\t" +email);
+                if (!cedula.isEmpty() && !nombre.isEmpty()
+                        && !dir.isEmpty() && !celular.isEmpty()
+                        && !email.isEmpty()) {
+                    register.println(cedula + "\t" + nombre + "\t" + dir + "\t" + celular + "\t" + email);
                 }
                 System.out.print("\n¿Desea agregar un nuevo cliente? (sí - No)\n[+]");
                 hayRegistro = read.nextLine();
@@ -52,71 +53,64 @@ public class App {
         }
     }
 
-    public static void FillBill (Scanner read, String filename) {
+    public static void FillBill(Scanner read, String filename) {
         try {
-            FileWriter outFile = new FileWriter(filename+ ".txt", false);
+            FileWriter outFile = new FileWriter(filename + ".txt", false);
             PrintWriter register = new PrintWriter(outFile);
-            
-
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void Leer(Scanner sc, String file_name){
+    public static void Ordenar(Scanner sc, String file_name) {
         boolean hay = false;
         String V[][] = new String[100][100];
-        String temp1[][] = new String[100][100];
-        int t=0;
-        int x=0;
+        String Temp[][] = new String[100][100];
+        int t = 0, x = 0;
 
-        while(hay == false){
+        while (hay == false) {
             try {
-                BufferedReader br = new BufferedReader(new FileReader(file_name+".txt"));
+                File Clientes = new File("Clientes.txt");
+                PrintWriter register_temp = new PrintWriter(new FileWriter(Clientes));
+                BufferedReader br = new BufferedReader(new FileReader(file_name + ".txt"));
                 String line = null;
                 String vec[] = new String[100];
 
-                while((line = br.readLine()) != null){
+                while ((line = br.readLine()) != null) {
                     String temp[] = line.split("\t");
-                    
-                    V[t][0] = temp[0];
-                    V[t][1] = temp[1];
-                    V[t][2] = temp[2];
-                    V[t][3] = temp[3];
-                    V[t][4] = temp[4];
+
+                    for (int k = 0; k < temp.length; k++) {
+                        V[t][k] = temp[k];
+                    }
+
                     vec[t] = V[t][0];
 
-                    t ++;
+                    t++;
                 }
-                // for (int k=0 ; k<t ;k++) {
-                //     for (int j=0 ; j<5 ; j++) {
-                //         System.out.print(V[k][j]+ " ");
-                //     }
-                //     System.out.println(" ");
-                // }
-                Ordenar(vec, t);
 
-                while (x <= t) {
-                    for (int k=0 ; k<t ; k++) {
-                        if (Integer.parseInt(vec[k]) == Integer.parseInt(V[k][0])) {
-                            temp1[x][0] = V[k][0];
-                            temp1[x][1] = V[k][1];
-                            temp1[x][2] = V[k][2];
-                            temp1[x][3] = V[k][3];
-                            temp1[x][4] = V[k][4];
+                Ordenar(vec, t);
+                // Buscar en la matriz el primer elemento del vector ordenado
+                while (x < t) {
+                    for (int k = 0; k < t; k++) {
+                        if (Integer.parseInt(vec[x]) == Integer.parseInt(V[k][0])) {
+                            for (int y = 0; y < t; y++) {
+                                Temp[x][y] = V[k][y];
+                            }
                         }
-                        
                     }
                     x++;
                 }
-                // for (int k=0 ; k<t ;k++) {
-                //     for (int j=0 ; j<5 ; j++) {
-                //         System.out.print(temp1[k][j]+ " ");
-                //     }
-                //     System.out.println(" ");
-                // }
+                // Creamos el archivo Clientes.txt ya ordenado
+                for (int k = 0; k < x; k++) {
+                    if (!Temp[k][0].isEmpty() && !Temp[k][1].isEmpty()
+                            && !Temp[k][2].isEmpty() && !Temp[k][3].isEmpty()
+                            && !Temp[k][4].isEmpty()) {
+                        register_temp.println(Temp[k][0] + "\t" + Temp[k][1] + "\t" + Temp[k][2] + "\t" + Temp[k][3]+ "\t" + Temp[k][4]);
+                    }
+                }
 
+                register_temp.close();
                 br.close();
                 hay = true;
 
@@ -125,17 +119,16 @@ public class App {
                 hay = false;
                 file_name = sc.nextLine(); // Archivo
             }
-        }   
+        }
     }
 
-    public static void Ordenar (String V[], int t) {
-
-        for (int k=0 ; k<t ; k++) {
-            for (int j=0 ; j<t-k-1 ; j++) {
-                if (Integer.parseInt(V[j+1]) < Integer.parseInt(V[j])) {
+    public static void Ordenar(String V[], int t) {
+        for (int k = 0; k < t; k++) {
+            for (int j = 0; j < t - k - 1; j++) {
+                if (Integer.parseInt(V[j + 1]) < Integer.parseInt(V[j])) {
                     String temp = V[j];
-                    V[j] = V[j+1];
-                    V[j+1] = temp;
+                    V[j] = V[j + 1];
+                    V[j + 1] = temp;
                 }
             }
         }
